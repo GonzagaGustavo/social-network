17242f48db9a9f8aae7f7a547e6a49fd00627e6b
Gustavo Gonzaga <Gustavogonzaga.gg243@gmail.com>
2023-06-03 16:13:22 -0300

    creating and reading user

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import http from "http";
+import { config } from "dotenv";
+import UserRoutes from "./routes/user";
+import bodyParser from "body-parser";
 
+config();
 const app = express();
 const server = http.createServer(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: "true" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/user", UserRoutes);
 
 server.listen(3001, () => console.log("> Server listening on port: 3001"));
---
+++ backend/src/routes/user/index.ts
@@ -0,0 +1,92 @@
+import { Router } from "express";
+import prisma from "../../db";
+import { hashSync } from "bcrypt";
+
+interface User {
+  id?: number;
+  name: string;
+  email: string;
+  password: string;
+  username: string;
+  phone: string;
+  bio: string;
+  gender: "male" | "female" | "prefer not to say";
+  birthday: Date;
+  country: string;
+  estate: string;
+  city: string;
+}
+
+const UserRoutes = Router();
+
+UserRoutes.post("/", async (req, res) => {
+  const user: User = {
+    name: req.body.name,
+    email: req.body.email,
+    bio: req.body.bio,
+    username: req.body.username,
+    birthday: new Date(req.body.birthday),
+    city: req.body.city,
+    estate: req.body.estate,
+    country: req.body.country,
+    gender: req.body.gender,
+    password: hashSync(req.body.password, 12),
+    phone: req.body.phone,
+  };
+
+  try {
+    await prisma.user.create({ data: user });
+    await prisma.$disconnect();
+
+    res.json({ success: true });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false, err: err });
+  }
+});
+
+UserRoutes.get("/", async (req, res) => {
+  const id = Number(req.query.id);
+  const username = req.query.username as string;
+  if (id || username) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: id ? id : undefined,
+          username: username ? username : undefined,
+        },
+        select: {
+          password: false,
+          country: false,
+          phone: false,
+          city: false,
+          estate: false,
+          email: true,
+          bio: true,
+          birthday: true,
+          gender: true,
+          id: true,
+          name: true,
+          username: true,
+        },
+      });
+      await prisma.$disconnect();
+
+      if (!user) {
+        res.status(404).send("user not found");
+        res.end();
+      } else {
+        res.status(200).json(user);
+        res.end();
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(501).json(err);
+    }
+  } else {
+    res.status(404).send("parameter id or username not found");
+    res.end();
+  }
+});
+
+export default UserRoutes;
